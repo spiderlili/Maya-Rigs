b160f236208cde7313dc3ba249fd52a1f38736a9
Jing <jtan1993@gmail.com>
2024-01-04 18:19:07 +0000

    Update hello_auto_rig_IK_joint_chain.py

--- auto_rigging_basics/hello_auto_rig_IK_joint_chain.py
+++ auto_rigging_basics/hello_auto_rig_IK_joint_chain.py
@@ -1,15 +1,19 @@
 # Create a simple IK arm
 import maya.cmds as cmds
 
-cmds.select(clear=True)
+def create_ik_arm():
+	cmds.select(clear=True)
 
-shoulder_jnt = cmds.joint(position = (0, 0, 4), name="shoulder_jnt")
-elbow_jnt = cmds.joint(position = (-2, 0, 0), name="elbow_jnt")
-wrist_jnt = cmds.joint(position = (0, 0, -4), name="wrist_jnt")
+	shoulder_jnt = cmds.joint(position = (0, 0, 4), name="shoulder_jnt")
+	elbow_jnt = cmds.joint(position = (-2, 0, 0), name="elbow_jnt")
+	wrist_jnt = cmds.joint(position = (0, 0, -4), name="wrist_jnt")
 
-# Point the X axis to the 1st child joint
-cmds.joint(shoulder_jnt, edit=True, orientJoint="xyz", zeroScaleOrient=True, secondaryAxisOrient="yup")
-cmds.joint(elbow_jnt, edit=True, orientJoint="xyz", zeroScaleOrient=True, secondaryAxisOrient="yup")
+	# Point the X axis to the 1st child joint
+	cmds.joint(shoulder_jnt, edit=True, orientJoint="xyz", zeroScaleOrient=True, secondaryAxisOrient="yup")
+	cmds.joint(elbow_jnt, edit=True, orientJoint="xyz", zeroScaleOrient=True, secondaryAxisOrient="yup")
 
-# Create an IK handle
-cmds.ikHandle(startJoint=shoulder_jnt, endEffector=wrist_jnt)
+	# Create an IK handle
+	cmds.ikHandle(startJoint=shoulder_jnt, endEffector=wrist_jnt)
+
+if __name__ == "__main__":
+	create_ik_arm()
