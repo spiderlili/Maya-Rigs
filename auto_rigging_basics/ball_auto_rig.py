7ca98c647a0dae3b357fa1ebba42463fc7154b9e
Jing Chun Tan <jtan1993@gmail.com>
2024-02-26 11:52:48 +0000

    add geo

--- auto_rigging_basics/ball_auto_rig.py
+++ auto_rigging_basics/ball_auto_rig.py
@@ -15,11 +15,15 @@ class BallAutoRig(object):
         root_grp = cmds.group(name=name, empty=True, world=True)
         anim_controls_grp = cmds.group(name="anim_controls", empty=True, parent=root_grp)
         geometry_grp = cmds.group(name="geometry_do_not_touch", empty=True, parent=root_grp) # Anything in this group should not be animated
+        ball_geo = self.create_ball("ball_geo", parent=geometry_grp)
 
         print("TODO: Construct Rig")
 
     def create_ball(self, name, parent=None):
-        return
+        ball_geo = cmds.sphere(pivot=(0,0,0), axis=(0,1,0), radius=1, name=name)[0]
+        if parent: # Check if a parent has been set
+            ball_geo = cmds.parent(ball_geo, parent)[0]
+        return ball_geo
     
 if __name__ == "__main__":
     cmds.file(newFile = True, force = True)
